b00c4025ac854bb53ecf904809aaedc1ae511bce
Anatoly1916 <dogsinspace1916@gmail.com>
2023-04-27 23:05:48 +0300

    feat: add section "portfolio" with fancybox

--- js/main.js
+++ js/main.js
@@ -12,4 +12,24 @@ $(document).ready(function(){
     $('.slider-prev').click(function() {
         $(".header-slider").trigger('prev.owl.carousel');
     });
+
+    // Fancybox
+    Fancybox.bind("[data-fancybox]", {
+        Thumbs: false,
+        Toolbar: {
+            display: {
+              left: ["infobar"],
+              middle: [
+                "zoomIn",
+                "zoomOut",
+                "toggle1to1",
+                "rotateCCW",
+                "rotateCW",
+                "flipX",
+                "flipY",
+              ],
+              right: ["slideshow", "thumbs", "close"],
+            },
+          },
+      });
   });
