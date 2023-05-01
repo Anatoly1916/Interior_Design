3791fed6c651ca01220183256abf10dc02b2f0fa
Anatoly1916 <dogsinspace1916@gmail.com>
2023-05-01 12:25:31 +0300

    feat: correct rows in js file

--- js/main.js
+++ js/main.js
@@ -33,39 +33,39 @@ $(document).ready(function(){
           },
       });
 
-      var number = document.querySelector('.number'),
-      numberTop = number.getBoundingClientRect().bottom,
-      start = +number.innerHTML, end = +number.dataset.max;
+    var number = document.querySelector('.number'),
+    numberTop = number.getBoundingClientRect().bottom,
+    start = +number.innerHTML, end = +number.dataset.max;
+      
+    window.addEventListener('scroll', function onScroll() {
+            if(window.pageYOffset > numberTop - window.innerHeight) {
+                this.removeEventListener('scroll', onScroll);
+            var interval = setInterval(function() {
+                    number.innerHTML = ++start;
+                if(start == end) {
+                        clearInterval(interval);
+                }
+            }, 25);
+        }
+    });
   
-  window.addEventListener('scroll', function onScroll() {
-          if(window.pageYOffset > numberTop - window.innerHeight) {
-              this.removeEventListener('scroll', onScroll);
-          var interval = setInterval(function() {
-                  number.innerHTML = ++start;
-              if(start == end) {
-                      clearInterval(interval);
-              }
-          }, 25);
-      }
-  });
   
+    var number2 = document.querySelector('.number2'),
+    numberTop2 = number2.getBoundingClientRect().bottom,
+    start2 = +number2.innerHTML, end2 = +number2.dataset.max;
   
-      var number2 = document.querySelector('.number2'),
-      numberTop2 = number2.getBoundingClientRect().bottom,
-     start2 = +number2.innerHTML, end2 = +number2.dataset.max;
-  
-  window.addEventListener('scroll', function onScroll() {
-          if(window.pageYOffset > numberTop2 - window.innerHeight) {
-              this.removeEventListener('scroll', onScroll);
-          var interval = setInterval(function() {
-                  number2.innerHTML = ++start2;
-              if(start2 == end2) {
-                      clearInterval(interval);
-              }
-          }, 5);
-      }
-  });
-  });
+    window.addEventListener('scroll', function onScroll() {
+            if(window.pageYOffset > numberTop2 - window.innerHeight) {
+                this.removeEventListener('scroll', onScroll);
+            var interval = setInterval(function() {
+                    number2.innerHTML = ++start2;
+                if(start2 == end2) {
+                        clearInterval(interval);
+                }
+            }, 5);
+        }
+    });
+});
 
 
 
