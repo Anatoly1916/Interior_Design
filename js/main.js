aecb5e2644323fc5f4b79bde1876a3d91719effc
Anatoly1916 <dogsinspace1916@gmail.com>
2023-05-01 11:33:33 +0300

    feat: finish desktop version

--- js/main.js
+++ js/main.js
@@ -32,4 +32,41 @@ $(document).ready(function(){
             },
           },
       });
+
+      var number = document.querySelector('.number'),
+      numberTop = number.getBoundingClientRect().top,
+      start = +number.innerHTML, end = +number.dataset.max;
+  
+  window.addEventListener('scroll', function onScroll() {
+          if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+              this.removeEventListener('scroll', onScroll);
+          var interval = setInterval(function() {
+                  number.innerHTML = ++start;
+              if(start == end) {
+                      clearInterval(interval);
+              }
+          }, 25);
+      }
   });
+  
+  
+      var number2 = document.querySelector('.number2'),
+      numberTop2 = number2.getBoundingClientRect().top,
+     start2 = +number2.innerHTML, end2 = +number2.dataset.max;
+  
+  window.addEventListener('scroll', function onScroll() {
+          if(window.pageYOffset > numberTop2 - window.innerHeight / 2) {
+              this.removeEventListener('scroll', onScroll);
+          var interval = setInterval(function() {
+                  number2.innerHTML = ++start2;
+              if(start2 == end2) {
+                      clearInterval(interval);
+              }
+          }, 5);
+      }
+  });
+  });
+
+
+
+
